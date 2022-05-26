51d7b9e0fa368b5483b09056d8e34ac616e88f24
kubicahere <kubica.here@gmail.com>
2022-05-26 19:21:56 +0200

    Commit changes

--- CityChooserAI/ViewModel/MainViewModel.cs
+++ CityChooserAI/ViewModel/MainViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CityChooserAI.ViewModel
 {
@@ -18,7 +19,77 @@ namespace CityChooserAI.ViewModel
             resultsPanel = new ResultsPanel(mainPanel);
             cityDataWindow = new CityDataWindow();
         }
+        #region Methods
+        public void AddChosenFeature(object sender)
+        {
+            if(mainPanel.singleTotalAttribute == null)
+            {
+                return;
+            }
+            if(mainPanel.singleTotalAttribute != null && mainPanel.singleChosenAttribute != null)
+            {
+                mainPanel.singleTotalAttribute = null;
+                mainPanel.singleChosenAttribute = null;
+                return;
+            }
+            mainPanel.chosenAttributes.Add(mainPanel.singleTotalAttribute);
+            mainPanel.totalAttributes.Remove(mainPanel.singleTotalAttribute);
+        }
+        public void RemoveChosenFeature(object sender)
+        {
+            if(mainPanel.singleChosenAttribute == null)
+            {
+                return;
+            }
+            if (mainPanel.singleTotalAttribute != null && mainPanel.singleChosenAttribute != null)
+            {
+                mainPanel.singleTotalAttribute = null;
+                mainPanel.singleChosenAttribute = null;
+                return;
+            }
+            mainPanel.totalAttributes.Add(mainPanel.singleChosenAttribute);
+            mainPanel.chosenAttributes.Remove(mainPanel.singleChosenAttribute);
+        }
+
+        #endregion
+
         #region ICommands
+        private ICommand _addFeatureClick = null;
+        public ICommand addFeatureClick
+        {
+            get
+            {
+                if (_addFeatureClick == null)
+                {
+                    _addFeatureClick = new RelayCommand(AddChosenFeature, arg => true);
+                }
+                return _addFeatureClick;
+            }
+        }
+        private ICommand _removeFeatureClick = null;
+        public ICommand removeFeatureClick
+        {
+            get
+            {
+                if(_removeFeatureClick == null)
+                {
+                    _removeFeatureClick = new RelayCommand(RemoveChosenFeature, arg => true);
+                }
+                return _removeFeatureClick;
+            }
+        }
+        private ICommand _checkClick = null;
+        public ICommand checkClick
+        {
+            get
+            {
+                if (_checkClick == null)
+                {
+                    _checkClick = new RelayCommand(resultsPanel.checkClick, arg => true);
+                }
+                return _checkClick;
+            }
+        }
 
 
         #endregion
