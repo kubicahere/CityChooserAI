d44599e3f7bdfdd3176ad938bf1ff8f66d501386
kubicahere <kubica.here@gmail.com>
2022-05-26 17:36:50 +0200

    Commit changes

--- CityChooserAI/ViewModel/MainViewModel.cs
+++ CityChooserAI/ViewModel/MainViewModel.cs
@@ -7,8 +7,20 @@ using System.Threading.Tasks;
 
 namespace CityChooserAI.ViewModel
 {
-    internal class MainViewModel : BaseViewModel
+    class MainViewModel : BaseViewModel
     {
+        public MainPanel mainPanel { get; set; }
+        public ResultsPanel resultsPanel { get; set; }
+        public CityDataWindow cityDataWindow { get; set; }
+        public MainViewModel()
+        {
+            mainPanel = new MainPanel();
+            resultsPanel = new ResultsPanel(mainPanel);
+            cityDataWindow = new CityDataWindow();
+        }
+        #region ICommands
 
+
+        #endregion
     }
 }
