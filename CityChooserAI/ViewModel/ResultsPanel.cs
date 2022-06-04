ccacd74136db3716d93cd46045a2d93a45e442f0
Venrofi <radoslaw.terelak@gmail.com>
2022-06-04 23:34:38 +0200

    Added Theme, Read of CSV file, operations on data and calculating the top5 scores

--- CityChooserAI/ViewModel/ResultsPanel.cs
+++ CityChooserAI/ViewModel/ResultsPanel.cs
@@ -3,10 +3,13 @@ using CityChooserAI.ViewModel.Base;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Diagnostics;
+using System.Globalization;
 
 namespace CityChooserAI.ViewModel
 {
@@ -56,12 +59,75 @@ namespace CityChooserAI.ViewModel
         #region Methods
         public void checkClick(object sender)
         {
-            if(MainPanel.chosenAttributes.Count < 3 || MainPanel.chosenAttributes.Count > 6)
+
+            if (MainPanel.chosenAttributes.Count < 3 || MainPanel.chosenAttributes.Count > 6)
             {
                 MessageBox.Show("Invalid amount of parameters!");
                 return;
             }
-            //TODO SET PATH TO .csv AND LOAD DATA TO FILE
+
+            Debug.WriteLine("Hello");
+
+            using (var reader = new StreamReader(@"QualityOfLifeDataFrame.csv")) // Add the .csv file to Relase folder!
+            {
+                List<string> P1 = new List<string>();
+                List<string> P2 = new List<string>();
+                List<string> P3 = new List<string>();
+                List<string> P4 = new List<string>();
+                List<string> P5 = new List<string>();
+                List<string> CityNames = new List<string>();
+                List<string> CountryNames = new List<string>();
+                List<string> Continents = new List<string>();
+                List<decimal> Score = new List<decimal>();
+
+                decimal P1_ratio = new decimal(1);
+                decimal P2_ratio = new decimal(0.8);
+                decimal P3_ratio = new decimal(0.6);
+                decimal P4_ratio = new decimal(0.4);
+                decimal P5_ratio = new decimal(0.2);
+
+                reader.ReadLine(); // skip first line with column description
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    P1.Add(values[4]); // TODO: Get the chosen column numbers
+                    P2.Add(values[5]);
+                    P3.Add(values[6]);
+                    P4.Add(values[7]);
+                    P5.Add(values[8]);
+                    CityNames.Add(values[1]);
+                    CountryNames.Add(values[2]);
+                    Continents.Add(values[3]);
+                }
+
+                for(int i = 0; i < CityNames.Count; i++)
+                {
+                    CultureInfo culture = new CultureInfo("en-US"); // for dots in values
+
+                    Score.Add((Convert.ToDecimal(P1[i], culture) * P1_ratio) + (Convert.ToDecimal(P2[i], culture) * P2_ratio)
+                        + (Convert.ToDecimal(P3[i], culture) * P3_ratio) + (Convert.ToDecimal(P4[i], culture) * P4_ratio) 
+                        + (Convert.ToDecimal(P5[i], culture) * P5_ratio));
+
+                    Debug.WriteLine(CityNames[i] + ": " + Score[i]);
+                }
+                var TOP5 = Score.OrderByDescending(x => x).Take(5).ToArray();
+
+                string resultMessage = "TOP 5:";
+
+                Debug.WriteLine("TOP 5:");
+                for(int i = 0; i < 5; i++)
+                {
+                    var index = Score.IndexOf(TOP5[i]);
+                    Debug.WriteLine(CityNames[index] + "," + CountryNames[index] + ", " + Continents[index] + ": " + TOP5[i]);
+                    resultMessage += "\n" + CityNames[index] + "," + CountryNames[index] + ", " + Continents[index] + ": " + TOP5[i];
+                }
+
+                MessageBox.Show(resultMessage);
+            }
+
             tmpList = new List<City>(cityList);
             resultPlayersList.Clear();
             // TODO AI FUNCTIONS HERE
